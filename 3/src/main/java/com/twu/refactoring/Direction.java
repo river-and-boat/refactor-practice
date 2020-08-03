465fa0e35ffe5f1e142f003b41e2e848c7ec3273
RiverAndBoat <842714673@qq.com>
2020-08-03 20:54:37 +0800

    use Map to replace switch case in 3

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -1,39 +1,41 @@
 package com.twu.refactoring;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Direction {
+
     private final char direction;
+
+    private static final Map<Character, List<Direction>> CHARACTER_MAP
+             = new HashMap<Character, List<Direction>>() {
+        {
+            put('N', Arrays.asList(new Direction('W'), new Direction('E')));
+            put('S', Arrays.asList(new Direction('E'), new Direction('W')));
+            put('E', Arrays.asList(new Direction('N'), new Direction('S')));
+            put('W', Arrays.asList(new Direction('S'), new Direction('N')));
+        }
+    };
 
     public Direction(char direction) {
         this.direction = direction;
     }
 
     public Direction turnRight() {
-        switch (direction) {
-            case 'N':
-                return new Direction('E');
-            case 'S':
-                return new Direction('W');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
-        }
+        return findDirection(direction).get(1);
     }
 
     public Direction turnLeft() {
-        switch (direction) {
-            case 'N':
-                return new Direction('W');
-            case 'S':
-                return new Direction('E');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
+        return findDirection(direction).get(0);
+    }
+
+    private List<Direction> findDirection(char direction) {
+        if (!CHARACTER_MAP.containsKey(direction)) {
+            throw new IllegalArgumentException();
+        } else {
+            return CHARACTER_MAP.get(direction);
         }
     }
 
