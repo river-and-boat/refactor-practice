7233d3d2176de961909c3390453c12f810ae5848
RiverAndBoat <842714673@qq.com>
2020-08-03 21:09:54 +0800

    replace with the function interface in 4

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,5 +1,7 @@
 package com.twu.refactoring;
 
+import java.util.function.Predicate;
+
 public class NumberCruncher {
     private final int[] numbers;
 
@@ -8,33 +10,26 @@ public class NumberCruncher {
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return countInMultipleSense(num -> num % 2 == 0);
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return countInMultipleSense(num -> num % 2 == 1);
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return countInMultipleSense(num -> num >= 0);
     }
 
     public int countNegative() {
+        return countInMultipleSense(num -> num < 0);
+    }
+
+    private int countInMultipleSense(Predicate<Integer> predicate) {
         int count = 0;
         for (int number : numbers) {
-            if (number < 0) count++;
+            if (predicate.test(number))
+                count++;
         }
         return count;
     }
